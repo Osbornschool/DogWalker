0fe8b992632d1d45627abe293c67666954eb0792
Osbornschool <osbornc@nycstudents.net>
2025-09-25 02:17:35 +0000

    FINAL

--- Dogwalker.java
+++ Dogwalker.java
@@ -1,7 +1,21 @@
 public class Dogwalker {
     private int maxDogs;
     private DogwalkCompany company;
-
+    public int dogWalkShift(int startHour, int endHour) {
+        int pay =0;
+        while(startHour <= endHour)
+        {
+            int dogs = Walkdogs(startHour);
+            pay += dogs * 5;
+            if (dogs == maxDogs || startHour >= 9 && startHour <= 17)
+            {
+                pay += 3;
+            }
+            startHour++;
+            return pay;
+        }
+        return pay;
+    }
     public Dogwalker(int max, DogwalkCompany comp) 
     {
     maxDogs = max;
@@ -21,4 +35,5 @@ public class Dogwalker {
             return maxDogs;
         }
     }
+     
 }
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,22 @@
 public class Main
 {
-    public static void main(String[] args)
-    {
-     DogwalkCompany c = new DogwalkCompany();
-     System.out.println(c);
-     int hour = c.addDogs();
-     System.out.println(c);
-     Dogwalker w = new Dogwalker(4, c);
-     System.out.println(w.Walkdogs(hour)+" dogs walked.");
-     System.out.println(c.numAvailableDogs(hour) + " dogs remaining");
-     for (int i = 7; i < 11; i++)
-     {
-        c.addDogs();
-     }
-    System.out.println(c);
-    }
-    Dogwalker w = new Dogwalker(3, c);
-    public int dogWalkShift(int startHour, int endHour)
-    {
-        int pay =0;
-        while(startHour <= endHour)
-        {
-            int dogs = Walkdogs(startHour);
-            pay += dogs * 5;
-            if (dogs == maxDogs || startHour >= 9 && startHour <= 17)
+    public static final DogwalkCompany c = null;
+        public static void main(String[] args)
             {
-                pay += 3;
-            }
-            startHour++;
-            return pay;
-        }
-        System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned");
-    }
-    
+ DogwalkCompany c = new DogwalkCompany();
+System.out.println(c);
+int hour = c.addDogs();
+System.out.println(c);
+ Dogwalker w = new Dogwalker(4, c);
+System.out.println(w.Walkdogs(hour)+" dogs walked.");
+System.out.println(c.numAvailableDogs(hour) + " dogs remaining");
+for (int i = 7; i < 11; i++)
+{
+ c.addDogs();
+}
+ System.out.println(c);
+    System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned"); 
+ }
 }
 
+                                                                                  
